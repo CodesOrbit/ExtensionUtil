8fb5fba1333b15a443c6739684d65d6a8841abf1
Zia <ziaainfo@gmail.com>
2019-12-14 15:56:09 +0500

    Initial commit with UILabel extension

---
+++ ExtensionUtil/ExtensionUtil.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionUtil.h
+//  ExtensionUtil
+//
+//  Created by Apple on 14/12/2019.
+//  Copyright © 2019 CodesOrbit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExtensionUtil.
+FOUNDATION_EXPORT double ExtensionUtilVersionNumber;
+
+//! Project version string for ExtensionUtil.
+FOUNDATION_EXPORT const unsigned char ExtensionUtilVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionUtil/PublicHeader.h>
+
+
